78abe058698986c87a4164de461a9f284ed3edcd
Engin Tekin <tekin.engin@yahoo.com>
2023-04-09 17:51:30 -0700

    add pa1

---
+++ README.md
@@ -0,0 +1,5 @@
+# cse101-pt.s23
+
+Grading scripts for Patrick Tantalo's CSE101 during Spring 2023 (Sections 01 and 02)
+
+Thanks to Andrew Gavgavian for his contributions.
---
+++ pa1/pa1-unit-check.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+SRCDIR=https://raw.githubusercontent.com/tekinengin/cse101-pt.s23/main/pa1
+
+if [ ! -e backup ]; then
+   echo "WARNING: a backup has been created for you in the \"backup\" folder"
+   mkdir backup
+fi
+
+cp *.c *.h Makefile backup   # copy all files of importance into backup
+
+curl $SRCDIR/ModelListTest.c > ModelListTest.c
+
+echo ""
+echo ""
+
+echo "Press Enter To Continue with ListTest Results"
+read verbose
+
+echo ""
+echo ""
+
+gcc -c -std=c17 -Wall -g ModelListTest.c List.c
+gcc -std=c17 -o ModelListTest ModelListTest.o List.o
+
+timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt 2> ListTest-mem.txt
+if [ $? -eq 124 ]; then
+    echo -e "${RED} ModelListTest TEST TIMED OUT ${NC}"
+fi
+
+cat ListTest-out.txt
+
+echo "List Valgrind Test: (press enter)"
+read garbage
+cat ListTest-mem.txt
+
+rm -f *.o ModelListTest* Lex ListTest-out.txt ListTest-mem.txt
+
